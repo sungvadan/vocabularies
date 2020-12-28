5e28fa82243e617d44558b67b6aa113f37b7ed76
sungvadan <svd.phan@gmail.com>
2020-12-28 16:45:35 +0100

    just me

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,6 +64,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        abort(403);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
