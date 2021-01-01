341da8d40114f77466619b4cbcf7c09590468e06
sungvadan <svd.phan@gmail.com>
2021-01-01 09:40:39 +0100

    reduce number of note

--- app/Services/RandomNoteService.php
+++ app/Services/RandomNoteService.php
@@ -10,17 +10,17 @@ class RandomNoteService
 
     public function getRandomNoteForUser(User $user): string
     {
-        $notes = Note::where('user_id', $user->id)->orderByRaw('rand()')->limit(3)->get();
+        $notes = Note::where('user_id', $user->id)->orderByRaw('rand()')->limit(2)->get();
         $randoms = [];
 
         foreach ($notes as $note) {
             $matches = explode('___', $note->body);
             $randoms[] = "# $note->title";
-            if (count($matches) <= 5) {
+            if (count($matches) <= 3) {
                 $randoms = array_merge($randoms ,$matches);
             } else {
                 $chunk = [];
-                while(count($chunk) < 5) {
+                while(count($chunk) <= 3) {
                     $randomIndex = random_int(0 , count($matches) - 1);
                     if (!in_array($randomIndex, $chunk)) {
                         $chunk[] = $randomIndex;
